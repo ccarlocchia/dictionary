b6924ee3a5fd42f897804941eb0451ad2b32dc78
Camila B. Carlocchia <ccarlocchia@gmail.com>
2022-02-21 13:50:13 -0300

    Updates

---
+++ precache-manifest.88a7b89cb3de483711a8d3247d3951b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca031271d9bac01ac2181c99feee4b58",
+    "url": "/dictionary/index.html"
+  },
+  {
+    "revision": "c1fe50dae76e9d3520b6",
+    "url": "/dictionary/static/css/main.3f4e4902.chunk.css"
+  },
+  {
+    "revision": "0de611116ccb7ef69aae",
+    "url": "/dictionary/static/js/2.cf8a18ab.chunk.js"
+  },
+  {
+    "revision": "c1fe50dae76e9d3520b6",
+    "url": "/dictionary/static/js/main.7ca6e4a4.chunk.js"
+  },
+  {
+    "revision": "67d3f92be30e1c42b517",
+    "url": "/dictionary/static/js/runtime~main.dbf1fe99.js"
+  }
+]);
